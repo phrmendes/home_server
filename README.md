f778fee4743203808fb50f6f7b67053385117291
Pedro Mendes <phrmendes@pop-os.localdomain>
2023-05-17 19:18:55 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Home server
+
+Configuration for my home server using an OrangePi Zero 2.
+
+## References
+
+- <https://github.com/containers/podman-compose>
+- <https://nginxproxymanager.com/>
+- <https://notthebe.ee/blog/easy-ssl-in-homelab-dns01/>
+- <https://podman.io/>
+- <https://www.duckdns.org/>
+- <https://yacht.sh/>
---
+++ setup.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+# variables
+USERNAME=phrmendes
+APT_PACKAGES=(python3-pip vim podman uidmap slirp4netns)
+PYTHON_PACKAGES=(podman-compose)
+
+# update system
+apt update && apt upgrade -y
+apt autoremove -y
+
+# create user
+useradd -m $USERNAME
+passwd $USERNAME
+usermod -aG sudo $USERNAME
+usermod --shell /bin/zsh $USERNAME
+
+# install dependencies
+apt install -y "${APT_PACKAGES[@]}"
+
+# change user
+su - $USERNAME
+
+# zsh theme
+omz theme set robbyrussell
+
+# install podman-compose
+pip3 install "${PYTHON_PACKAGES[@]}"
+
+# add local bin to path
+echo "export PATH=$PATH:$HOME/.local/bin" >>"$HOME/.zshrc"
+source "$HOME/.zshrc"
+
+# remove ssh root login
+sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
